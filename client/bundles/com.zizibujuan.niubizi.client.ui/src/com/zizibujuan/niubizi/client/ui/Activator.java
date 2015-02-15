d6fc8bfd15563a365f59bc2d2e19db593fc9feba
xiaohulu <idocument@qq.com>
2015-02-15 23:08:58 +0800

    程序启动时创建托管的文件夹

--- client/bundles/com.zizibujuan.niubizi.client.ui/src/com/zizibujuan/niubizi/client/ui/Activator.java
+++ client/bundles/com.zizibujuan.niubizi.client.ui/src/com/zizibujuan/niubizi/client/ui/Activator.java
@@ -1,7 +1,13 @@
 package com.zizibujuan.niubizi.client.ui;
 
+import java.io.File;
+
+import org.osgi.service.prefs.Preferences;
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.prefs.PreferencesService;
 
 public class Activator implements BundleActivator {
 
@@ -19,6 +25,19 @@ public class Activator implements BundleActivator {
 		Activator.context = bundleContext;
 		
 		// TODO:插入系统默认的标签
+		
+		Preferences preferences = ConfigurationScope.INSTANCE.getNode("com.zizibujuan.niubizi.client.ui");
+		String homeDir = preferences.get("niubizi.home.dir", null);
+		if(homeDir == null){
+			homeDir = System.getProperty("user.home") + System.getProperty("file.separator") + ".niubizi";
+			preferences.put("niubizi.home.dir", homeDir);
+			preferences.flush();
+		}
+		
+		// 判断文件夹是否已存在,若不存在则创建
+		if(!new File(homeDir).exists()){
+			new File(homeDir).mkdirs();
+		}
 	}
 
 	/*
